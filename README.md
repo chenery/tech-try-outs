cc403bdeebe256542586ef6142f630d30274e465
Paul Chenery <paulchenery@gmail.com>
2012-08-14 08:50:38 +0100

    Add orm for mysql and date support

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Note: Can try out using Bootstrap as per [https://github.com/shomeya/bootstrap-s
 
 	$ node app
 
-Add a module to make http client requests [https://github.com/danwrong/restler].
+Add a module to make http client requests [https://github.com/danwrong/restler](https://github.com/danwrong/restler).
 
     $ npm install util
     $ npm install restler
@@ -26,7 +26,7 @@ Add a module to make http client requests [https://github.com/danwrong/restler].
     var sys = require('util'),
         rest = require('restler');
 
-A node debugger [https://github.com/dannycoates/node-inspector].
+A node debugger [https://github.com/dannycoates/node-inspector](https://github.com/dannycoates/node-inspector).
 
     $ npm install -g node-inspector
 
@@ -37,3 +37,15 @@ To enable debug mode for the program:
 Start the inspector in the background.
 
     $ node-inspector &
+
+Add in mysql client support [https://github.com/felixge/node-mysql](https://github.com/felixge/node-mysql)
+
+    $ npm install mysql
+
+Add in ORM support so we can persist https://github.com/dresende/node-orm/.
+
+    $ npm install orm
+
+Add in a date lib to format dates to insertion into the database http://momentjs.com/.
+
+    $ npm install moment
--- node-app-1/routes/index.js
+++ node-app-1/routes/index.js
@@ -7,29 +7,75 @@ exports.index = function(req, res){
 
     // placeholder area to try out running server side JS
 
+    // add in orm
+    var orm = require("orm");
     // add in restler
     var sys = require('util');
     var rest = require('restler');
+    // date support
+    var moment = require('moment');
 
-    rest.get('http://content.guardianapis.com/search?section=commentisfree&format=json').on('complete', function(data) {
+    var db = orm.connect("mysql://root:spit69fire@localhost/node_news", function (success, db) {
+        if (!success) {
+            console.log("Could not connect to database!");
+            return;
+        }
 
-        if (data instanceof Error) {
-            sys.puts('Error: ' + data.message);
-            console.log("Error returned " + data.message);
-        } else {
-            sys.puts(data);
-            console.log("Received data first " + data.response.results.length + " of " + data.response.total);
+        // you can now use db variable to define models
 
-            var results = data.response.results;
+        var News = db.define("news", {
+            "storyId"   : { "type": "string" },
+            "pubDate"   : { "type": "date" },
+            "title"     : { "type": "string"},
+            "url"       : { "type": "string"}
+        });
 
-            if (typeof results !== "undefined") {
-                // iterate the results and store then in the database
-                for(var i = 0; i < results.length ; i++) {
-                    var result = results[i];
-                    console.log("Story: " + result.id + " " + result.webPublicationDate + " " + result.webTitle + " " + result.webUrl);
+        // create the tables if they don't exist
+        News.sync();
+
+        // grab some data to store
+        rest.get('http://content.guardianapis.com/search?section=commentisfree&format=json').on('complete', function(data) {
+
+            if (data instanceof Error) {
+                sys.puts('Error: ' + data.message);
+                console.log("Error returned " + data.message);
+            } else {
+                sys.puts(data);
+                console.log("Received data first " + data.response.results.length + " of " + data.response.total);
+
+                var results = data.response.results;
+
+                if (typeof results !== "undefined") {
+                    // iterate the results and store then in the database
+                    for(var i = 0; i < results.length ; i++) {
+                        var result = results[i];
+                        console.log("Story: " + result.id + " " + result.webPublicationDate + " " + result.webTitle
+                            + " " + result.webUrl);
+
+                        // parse the pub date and produce a native js date
+                        var pubDate = moment(result.webPublicationDate).toDate();
+                        console.log("parsed date result: " + pubDate);
+
+                        // wack in the data
+                        var story = new News({
+                            "storyId" : result.id,
+                            "pubDate" : pubDate,
+                            "title"   : result.webTitle,
+                            "url" : result.webUrl
+                        });
+
+                        story.save(function (err, itemCopy) {
+                            if (!err) {
+                                console.log("Saved! ID=" + itemCopy.id);
+                            } else {
+                                console.log("Something went wrong...");
+                                console.dir(err);
+                            }
+                        });
+                    }
                 }
             }
-        }
+        });
     });
 
   res.render('index', { title: 'Express' });
